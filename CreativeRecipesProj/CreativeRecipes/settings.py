e3bdb6e8ba901c26c6ac2acd7ba88843ce51e783
Joy <joy.t.quan@gmail.com>
2020-07-06 18:17:21 -0400

    made first model

--- CreativeRecipesProj/CreativeRecipes/settings.py
+++ CreativeRecipesProj/CreativeRecipes/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipes.apps.RecipesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes',
 ]
 
 MIDDLEWARE = [
--- CreativeRecipesProj/recipes/models.py
+++ CreativeRecipesProj/recipes/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# note: change model to use ForeignKey
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=100)
+    recipe_image = models.ImageField()
+    prep_time = models.DurationField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+    class TypeOfFood(models.TextChoices):
+        APPETIZER = 'AP', _('Appetizer')
+        ENTREE = 'EN', _('Entree')
+        DESSERT = 'DS', _('Dessert')
+        DRINK = 'DR', _('Drink')
+        SNACK = 'SN', _('Snack')
+
+    type_of_food = models.CharField(
+        max_length=2,
+        choices=TypeOfFood.choices,
+        default=TypeOfFood.ENTREE,
+    )
+
+    class Availability(models.TextChoices):
+        STAPLE = 'ST', _('Staple')
+        SPECIAL = 'SP', _('Special')
+
+    availability = models.CharField(
+        max_length=2,
+        choices=Availability.choices,
+        default=Availability.STAPLE,
+    )
+    
+    def is_staple(self):
+        return self.availability is self.Availability.STAPLE
+    
+        
